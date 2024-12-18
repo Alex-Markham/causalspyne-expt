1ade8b2c94b2d86db7cac6210c7a31f512faa8d8
smilesun <smilesun.east@gmail.com>
2024-12-18 12:27:46 +0100

    add hint on how to fix qt6 by uv upgrade to downgrade qt6 version

--- hint.sh
+++ hint.sh
@@ -2,6 +2,8 @@
 # uv will be responsible for updating all dependencies in the benchmark, the uv.lock file need to be pushed each time
 uv sync   # ensures my environment matches the uv.lock file
 uv lock --upgrade-package causalSpyne  # <package> does not have to be in pip, this is specified in pyproject.toml
+# e.g. uv lock --upgrade-package pyqt6-qt6==6.7.1 to downgrade 6.8.0 to 6.7.1 to avoid platform
+# consistency problems.
 # [tool.uv.sources]
 # causalspyne = { git = "https://github.com/marrlab/causalspyne" }
 snakemake all # don't use this
