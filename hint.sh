72318f72601dbf23b310b35f12c9c43f5233bfe4
smilesun <smilesun.east@gmail.com>
2024-12-18 12:21:31 +0100

    add hint.sh

---
+++ hint.sh
@@ -0,0 +1,13 @@
+# pip install uv
+# uv will be responsible for updating all dependencies in the benchmark, the uv.lock file need to be pushed each time
+uv sync   # ensures my environment matches the uv.lock file
+uv lock --upgrade-package causalSpyne  # <package> does not have to be in pip, this is specified in pyproject.toml
+# [tool.uv.sources]
+# causalspyne = { git = "https://github.com/marrlab/causalspyne" }
+snakemake all # don't use this
+uv run snakemake all   # this does not require installation of snakemake, uv will create a temporary virtural environment
+#uv add numpy will add new packages
+#uv add causaulspyne@git.come
+
+# final results live in the "results" directory
+# in the workflow diretory you can find the snakefile
