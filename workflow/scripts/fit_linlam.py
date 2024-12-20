5f81429c8c505178b3e142a5c433a6f696286d1f
Alex Markham <alex.markham@causal.dev>
2024-12-20 12:48:40 +0100

    fix: return order over observed nodes in linlam; #1

--- workflow/scripts/fit_linlam.py
+++ workflow/scripts/fit_linlam.py
@@ -5,8 +5,14 @@ from causallearn.search.HiddenCausal.GIN.GIN import GIN
 dataset = np.loadtxt(str(snakemake.input.dataset), delimiter=",")[1:]
 
 # Run LiNLAM algorithm
-graph, causal_order = GIN(dataset)
+graph, latent_causal_order = GIN(dataset)
+
+# causal order of labels of observed nodes
+causal_order = graph.get_causal_ordering()
+node_map = graph.get_node_map()
+obs_causal_order = [node_map[node] for node in causal_order]
+
 
 # save outputs
 np.savetxt(snakemake.output["graph"], graph.graph, delimiter=",", fmt="%d")
-np.savetxt(snakemake.output["causal_order"], causal_order, delimiter=",", fmt="%d")
+np.savetxt(snakemake.output["causal_order"], obs_causal_order, delimiter=",", fmt="%d")
