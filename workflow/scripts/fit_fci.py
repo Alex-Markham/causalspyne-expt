96b98d37cf3b9de4f4e77ed3f411b931d7bb6c76
Alex Markham <alex.markham@causal.dev>
2024-12-20 12:41:21 +0100

    fix: return labels instead of node objects for fci causal order; #1

--- workflow/scripts/fit_fci.py
+++ workflow/scripts/fit_fci.py
@@ -7,7 +7,12 @@ dataset = np.loadtxt(str(snakemake.input.dataset), delimiter=",")[1:]
 # Run FCI algorithm
 g, edges = fci(dataset)
 pag = g.graph
+
+# get labels of causal order of nodes
 causal_order = g.get_causal_ordering()
+node_map = g.get_node_map()
+causal_order = [node_map[node] for node in causal_order]
+
 
 # save outputs
 np.savetxt(snakemake.output["pag"], pag, delimiter=",", fmt="%d")
