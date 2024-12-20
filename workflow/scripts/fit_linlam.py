350b34e696ce405d0ce7e7bfc58aacc9d5c14ce8
Alex Markham <alex.markham@causal.dev>
2024-12-20 14:27:07 +0100

    Update fit_linlam.py

--- workflow/scripts/fit_linlam.py
+++ workflow/scripts/fit_linlam.py
@@ -1,9 +1,12 @@
 import numpy as np
+import pickle
 from causallearn.search.HiddenCausal.GIN.GIN import GIN
 from causalspyne.utils_causallearn_g2ancestral import get_causal_order
 
 # Load the dataset
 dataset = np.loadtxt(str(snakemake.input.dataset), delimiter=",")[1:]
+with open(snakemake.input["node_names"], "rb") as inp:
+    node_names = pickle.load(inp)
 
 # Run LiNLAM algorithm
 graph, latent_causal_order = GIN(dataset)
