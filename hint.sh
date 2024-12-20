6e2c0206e837fff18aeebd79254fb25bd78f9ca3
smilesun <smilesun.east@gmail.com>
2024-12-20 17:30:52 +0100

    update hint.sh

--- hint.sh
+++ hint.sh
@@ -1,5 +1,6 @@
 # pip install uv
 # uv will be responsible for updating all dependencies in the benchmark, the uv.lock file need to be pushed each time
+python3 -m uv sync  # for conda-free python 
 uv sync   # ensures my environment matches the uv.lock file
 uv lock --upgrade-package causalSpyne  # <package> does not have to be in pip, this is specified in pyproject.toml
 # e.g. uv lock --upgrade-package pyqt6-qt6==6.7.1 to downgrade 6.8.0 to 6.7.1 to avoid platform
