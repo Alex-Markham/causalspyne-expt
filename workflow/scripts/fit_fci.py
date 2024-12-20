145fe36eb8ae9fa62d75217a1a911c895632a7d0
Alex Markham <alex.markham@causal.dev>
2024-12-20 12:30:47 +0100

    feat: produce fci outputs for ancestral accuracy comparison; issue #1

--- workflow/scripts/fit_fci.py
+++ workflow/scripts/fit_fci.py
@@ -1,5 +1,5 @@
-from causallearn.search.ConstraintBased.FCI import fci
 import numpy as np
+from causallearn.search.ConstraintBased.FCI import fci
 
 # Load the dataset
 dataset = np.loadtxt(str(snakemake.input.dataset), delimiter=",")[1:]
@@ -7,6 +7,8 @@ dataset = np.loadtxt(str(snakemake.input.dataset), delimiter=",")[1:]
 # Run FCI algorithm
 g, edges = fci(dataset)
 pag = g.graph
+causal_order = g.get_causal_ordering()
 
-# save pag
+# save outputs
 np.savetxt(snakemake.output["pag"], pag, delimiter=",", fmt="%d")
+np.savetxt(snakemake.output["causal_order"], causal_order, delimiter=",", fmt="%d")
