a26b0e9675a6cac49dcdcc920b65dfe399373e5c
Xudong Sun <smilesun@users.noreply.github.com>
2024-12-23 09:27:29 +0100

    Update hint.sh

--- hint.sh
+++ hint.sh
@@ -12,5 +12,8 @@ uv run snakemake all   # this does not require installation of snakemake, uv wil
 #uv add numpy will add new packages
 #uv add causaulspyne@git.come
 
+
+uv run snakemake --rerun-incomplete -j 10 to start again 
+
 # final results live in the "results" directory
 # in the workflow diretory you can find the snakefile
