f4e93372f9184957366f8de2c51c2c008c2e446e
Alex Markham <alex.markham@causal.dev>
2025-01-08 13:56:55 +0100

    fix: bug in shd plot

--- workflow/scripts/boxplot_shd.py
+++ workflow/scripts/boxplot_shd.py
@@ -1,17 +1,21 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-
 
 shd_results = pd.read_csv(snakemake.input["csv"])
 
-plt.figure(figsize=(12, 6))
-sns.boxplot(x="graph_size", y="shd", data=shd_results, hue="method")
-plt.title("")
-plt.xlabel("")
-plt.ylabel("Structural Hamming Distance (SHD)")
-
-plt.savefig(snakemake.output[0])
+plot = sns.catplot(
+    data=shd_results,
+    x="graph_size",
+    y="shd",
+    hue="method",
+    log_scale=False,
+    legend=True,
+    palette="Set2",
+    row="alg",
+    kind="box",
+)
+plot.figure.savefig(snakemake.output[0])
 
 # small = shd_results[shd_results["graph_size"] == "small"]
 # small["dag_size"].mean()
