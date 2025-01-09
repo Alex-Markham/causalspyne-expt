877b411d4e7d871420e80889fabe48351e56c8d8
Xudong Sun <smilesun@users.noreply.github.com>
2025-01-09 12:26:12 +0100

    Update fit_linlam.py

--- workflow/scripts/fit_linlam.py
+++ workflow/scripts/fit_linlam.py
@@ -2,7 +2,7 @@ import pickle
 
 import numpy as np
 from causallearn.search.HiddenCausal.GIN.GIN import GIN
-from causalspyne.utils_causallearn_g2ancestral import get_causal_order
+from causalspyne.utils_causallearn_g2ancestral import get_causalearn_order
 
 # Load the dataset
 dataset = np.loadtxt(str(snakemake.input.dataset), delimiter=",")[1:]
@@ -12,7 +12,7 @@ with open(snakemake.input["node_names"], "rb") as inp:
 # Run LiNLAM algorithm
 graph, latent_causal_order = GIN(dataset)
 
-obs_causal_order, order_latent = get_causal_order(graph, node_names)
+obs_causal_order, order_latent = get_causalearn_order(graph, node_names)
 obs_causal_order = [
     int(x_str[1:]) for x_str in obs_causal_order
 ]  # strip X and convert to int for labels like "X1"
