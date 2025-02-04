42053fa20d23d4cd31c4ce45fc2515faa71074a7
smilesun <smilesun@gmail.com>
2025-02-04 11:59:46 +0100

    .

--- run_benchmark_slurm.sh
+++ run_benchmark_slurm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=cpu_p
+#SBATCH --qos=cpu_normal
+#SBATCH --gres=cpu:1
+#SBATCH --nice=10000
+#SBATCH -t 24:00:00
+#SBATCH -c 2
+#SBATCH --mem=10G
+
 export logdir="slurm_logs/"
 echo "slurm logs going into ${logdir}"
 uv run snakemake --profile "config.yaml" --keep-going --keep-incomplete --notemp
