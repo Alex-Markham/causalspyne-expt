a478f78f7e6919fcd24e12353acb02e9f0517328
Alex Markham <alex.markham@causal.dev>
2025-02-06 11:46:48 +0100

    Update run_benchmark_slurm.sh

--- run_benchmark_slurm.sh
+++ run_benchmark_slurm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 #SBATCH --partition=cpu_p
 #SBATCH --qos=cpu_normal
 #SBATCH --nice=10000
 #SBATCH -t 24:00:00
 #SBATCH -c 2
 #SBATCH --mem=10G
+#SBATCH --job-name=causalspyne
 
-uv run snakemake --profile "./slurm" --keep-going --keep-incomplete
+uv run snakemake --profile ./slurm --keep-going --rerun-incomplete all
