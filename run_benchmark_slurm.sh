315cfa052ec878d720f1ade5a257927aea6a5091
smilesun <smilesun@gmail.com>
2025-02-06 14:35:11 +0100

    .

--- run_benchmark_slurm.sh
+++ run_benchmark_slurm.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-#SBATCH --partition=cpu_p
-#SBATCH --qos=cpu_normal
-#SBATCH --nice=10000
-#SBATCH -t 24:00:00
-#SBATCH -c 2
-#SBATCH --mem=10G
-#SBATCH --job-name=causalspyne
-
 uv run snakemake --profile ./slurm --keep-going --rerun-incomplete all
